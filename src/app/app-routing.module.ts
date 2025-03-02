f53915cdbec6406198fc2ec7df6863f39bcd7bd6
Asteriskzkx <assassinaom@gmail.com>
2025-03-02 20:13:32 +0700

    6th commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { MovieComponent } from './components/movie/movie.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'homepage', component: HomepageComponent },
+  { path: 'movie/:title', component: MovieComponent },
   { path: '', redirectTo: '/homepage', pathMatch: 'full' },
 ];
 
--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from '../../services/movie.service';
+import { Movie } from '../../models/movie.model';
 
 @Component({
   selector: 'app-movie',
   standalone: false,
   templateUrl: './movie.component.html',
-  styleUrl: './movie.component.css'
+  styleUrl: './movie.component.css',
 })
-export class MovieComponent {
+export class MovieComponent implements OnInit {
+  movie!: Movie;
 
+  constructor(
+    private route: ActivatedRoute,
+    private movieService: MovieService
+  ) { }
+
+  ngOnInit(): void {
+    const title = this.route.snapshot.paramMap.get('title');
+    if (title) {
+      this.movie = this.getMovieByTitle(title);
+    }
+  }
+
+  getMovieByTitle(title: string): Movie {
+    let allMovies: Movie[] = [
+      ...this.movieService.nowInTheaters,
+      ...this.movieService.upcomingMovies,
+    ];
+    const foundMovie = allMovies.find((movie) => movie.title === title);
+    if (foundMovie) {
+      return foundMovie;
+    } else {
+      return this.movieService.nowInTheaters[0];
+    }
+  }
 }
